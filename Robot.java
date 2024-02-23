b24f3933af207022b6931d626df0770714ef4e07
SomeGuy3793 <150858527+SomeGuy3793@users.noreply.github.com>
2024-02-22 19:16:57 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -4,18 +4,20 @@
 
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-// https://software-metadata.revrobotics.com/REVLib-2024.json
+import edu.wpi.first.wpilibj.XboxController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.RelativeEncoder;
-
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkRelativeEncoder;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.compound.Diff_DutyCycleOut_Position;
 //import edu.wpi.first.wpilibj.ADXRS450_Gyro; // small FRC gyro in SPI slot
 // https://dev.studica.com/releases/2024/NavX.json
 import com.kauailabs.navx.frc.AHRS;
@@ -23,28 +25,24 @@ import edu.wpi.first.wpilibj.SPI;
 
 //dunno
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class,
- * specifically it contains
- * the code necessary to operate a robot with tank drive.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
 
-  ////////////////////////////////////////////////////////////////////////////
-  // Constants we use //
-  ////////////////////////////////////////////////////////////////////////////
-  // We use 2 Xbox game pads to run the robot, 1 for the driver, 1 for the
-  // operator. The driver usually just maneuvers the robot around the arena.
-  // The operator manipulates the extra stuff for that year's robot (like
-  // shooters, arms, winches ...).
-  // "raw" mappings for the Xbox controller (and the clones we bought) below.
-  // Following are for buttons that are either
-  // pressed (getRawButton returns true) or not (getRawButton returns false).
-  // XboxController has unique names for everything, but using the
-  // lower level GenericHID raw reads lets us switch buttons quicker.
+  //motor speeds
+  public static final int kClimberMotorSpd = 0;
+  public static final int kItems = 1;
+  
+  //controllers
   public static final int kAButton = 1;
   public static final int kBButton = 2;
   public static final int kXButton = 3;
@@ -56,35 +54,7 @@ public class Robot extends TimedRobot {
   public static final int kLeftStickPress = 9;
   public static final int kRighttStickPress = 10;
 
-  // following are for triggers/joy sticks
-  // getRawAxis returns values from -1.0000 to 1.0000
-  public static final int kLeftStickXaxis = 0;
-  public static final int kleftStickYaxis = 1;
-  public static final int kLeftTrigger = 2; // 0.00 to 1.00
-  public static final int kRightTrigger = 3; // 0.00 to 1.00
-  public static final int kRightStickXaxis = 4;
-  public static final int kRightStickYaxis = 5;
-
-  public static final int kIntakeCurrentLimit = 5;
-  
-
-  public static final double kDeadZone = 0.2;
-
-  // The following constants are array indices for tuning values used in auto
-  public static final int kClimberSpd = 0;
-  public static final int kRotateSpd = 1;
-  public static final int kShooterSpd = 2;
-  public static final int kIntakeSpd = 3;
-  public static final int kItems = 4; // set to last one above + 1
-
-  // Sometimes the game controller triggers and joy sticks lie and give a non-0
-  // value even though they aren't being touched. Filter out any reading less
-  // than the kDeadZone.
-  
-  double power = 0.2;
-
-   ////////////////////////////////////////////////////////////////////////////
-  // Global variables //
+// Global variables //
   ////////////////////////////////////////////////////////////////////////////
   // following is the current index (will range from 0 to kItems - 1
   public int tCurIndex = 0;
@@ -102,167 +72,93 @@ public class Robot extends TimedRobot {
   int quadrant = 1;
   double direction = 1; // otherwise -1
   int whichMotor = 0; // 0 is pivot 1 is roll motor
-
-  int allow5 = 0;
-  int allow6 = 0;
-
+  
   public static final int kDriver = 0;
   public static final int kOperator = 1;
   
+
+  CANSparkMax climberA = new CANSparkMax(34, MotorType.kBrushless);
+//CANSparkMax climberB = new CANSparkMax(0, MotorType.kBrushless);
+
   
   
-  CANSparkMax climberArm = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax shooter = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax intake = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax rotater = new CANSparkMax(0, MotorType.kBrushless);
-
-
-
-  //reed switch thingymabob doing doing hoing soing boing
-  //false = closed. Use in if statements. false by default
-  DigitalInput climberSwitchClosed = new DigitalInput(0);
-  DigitalInput rotaterSwitchClosed = new DigitalInput(0);
-
-//replace with roborio DIO
-
-//encoders or whatever -----------------------------------
-  //RelativeEncoder climberArmLocation;
+  
   
 
-  // Gyro gyro = new ADXRS450_Gyro();
-  //AHRS gyro = new AHRS(SPI.Port.kMXP);
+   //false = closed. Use in if statements. false by default
+   
+   //DigitalInput climberSwitchMin = new DigitalInput(0);
 
-  // double m_chassisAngularOffset = 0;
-  // SwerveModuleState m_desiredState = new SwerveModuleState(0.0, new
-  // Rotation2d());
+   //replace with roborio DIO
 
-  XboxController driverController = new XboxController(0); // USB port 0
-  XboxController operatorController = new XboxController(1); // USB port 1
+   XboxController driverController = new XboxController(0); // USB port 0
+   XboxController operatorController = new XboxController(1); // USB port 1
 
-  // Use the following array so we can easily map the operator controls to
-  // either controller. Doing this so it is easy to map both to one
-  // controller in an emergency (like operator gets sick). Of course you
-  // need to not use the same buttons, sliders/sticks on both pads.
-  // The mapping to the array happens down in robotInit.
-  public XboxController[] Controller = new XboxController[2];
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
 
-  int last_press = 0; 
-  int intake_counter = 0;
+   //back up and easy mapping or smth
+   public XboxController[] Controller = new XboxController[2];
+
+   RelativeEncoder climberEncoderA=climberA.getEncoder(SparkRelativeEncoder.Type.kHallSensor,42);
+// RelativeEncoder climberEncoderB=climberB.getEncoder(SparkRelativeEncoder.Type.kHallSensor,42);
+
 
   @Override
   public void robotInit() {
-    //gyro.reset();
-
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    // m_rightMotor.setInverted(true);
-
     Controller[kDriver] = driverController;
     Controller[kOperator] = operatorController;
 
-      int index;
-    //SmartDashboard.putNumber("yaw", gyro.getAngle());
-    // initialize arrays used to tune values used in auto
-   
+    
+     
+    
 
-    index = kClimberSpd;
-    taCurValue[index] = 1;
-    taDelta[index] = .01;
-    taLabel[index] = "Climber Speed";
-
-    index = kRotateSpd;
-    taCurValue[index] = .5;
-    taDelta[index] = .01;
-    taLabel[index] = "shooter rotate speed";
-
-    index = kShooterSpd;
-    taCurValue[index] = 1.0;
-    taDelta[index] = 0.01;
-    taLabel[index] = "shooter speed";
-
-    index = kIntakeSpd;
-    taCurValue[index] = 1.0;
-    taDelta[index] = 0.01;
-    taLabel[index] = "intake speed";
-
+    
+    int index;
+  //SmartDashboard.putNumber("yaw", gyro.getAngle());
+  // initialize arrays used to tune values used in auto
+  index = kClimberMotorSpd;
+  taCurValue[index] = .20;
+  taDelta[index] = 0.01;
+  taLabel[index] = "arm motor speed";
   }
 
-    
-    
-  
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
 
+   
   @Override
   public void robotPeriodic() {
-    
+    SmartDashboard.putNumber("Climber Encoder A", climberEncoderA.getPosition());
+    //SmartDashboard.putNumber("Climber Encoder B", climberEncoderB.getPosition());
+
   }
+
+  
+
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
 
-    
+    if (Controller[kDriver].getRawButton(kXButton))
+    climberA.set(kClimberMotorSpd);
+   else
+    climberA.set(0);
 
-    if (Math.abs(Controller[kDriver].getRawAxis(kleftStickYaxis))  > kDeadZone){
-      shooter.set(Controller[kDriver].getRawAxis(kleftStickYaxis) * taCurValue[kShooterSpd]);
-      intake.set(Controller[kDriver].getRawAxis(kleftStickYaxis) * taCurValue[kIntakeSpd]);
-  }else{
-      shooter.set(0);
+    if (Controller[kDriver].getRawButton(kYButton) && climberEncoderA.getPosition()<=0)
+    climberA.set(-kClimberMotorSpd);
+   else
+    climberA.set(0);
+
   }
-    if (Math.abs(Controller[kDriver].getRawAxis(kRightStickYaxis))  > kDeadZone)
-      intake.set(-(Controller[kDriver].getRawAxis(kRightStickYaxis) * (taCurValue[kIntakeSpd]*2)));
-    else
-      intake.set(0);
 
-      if (Controller[kDriver].getRawButton(kAButton)) 
-       rotater.set(0);
-      else
-       rotater.set(0);
-
-    if (Controller[kDriver].getRawButton(kBButton) && rotaterSwitchClosed.get()==false) 
-       rotater.set(kRotateSpd);
-      else
-       rotater.set(0);
-
-      if (Controller[kDriver].getRawButton(kXButton) && climberSwitchClosed.get() == false)
-       climberArm.set(kClimberSpd);
-      else
-       climberArm.set(0);
-
-       if (Controller[kDriver].getRawButton(kYButton) && climberSwitchClosed.get() == false)
-       climberArm.set(-kClimberSpd);
-      else
-       climberArm.set(0);
-  }
-    
-/* 
-    if (Controller[kDriver].getRawButton(kAButton) && intake_counter <= 0){
-      last_press ++;
-      intake_counter = 40;
-      if (last_press > 3)
-        last_press = 0;
-    }
-    
-
-    if (!Controller[kDriver].getRawButton(kAButton))
-      intake_counter --;
-
-
-    if (last_press == 0)
-      intake.set(0);
-    else if (last_press == 1)
-      intake.set(taCurValue[kIntakeMotorSpd]);
-    else if (last_press == 2)
-      intake.set(0);
-    else if (last_press == 3 )
-      intake.set(-taCurValue[kIntakeMotorSpd]);
-
-    SmartDashboard.putNumber("last_press",last_press);
-    SmartDashboard.putNumber("intake_counter",intake_counter);    
-  } 
-*/
-   /////////////////////////////////////////////////////////////////////////////
-  // start of auto tuning routines
-  /////////////////////////////////////////////////////////////////////////////
-  // delta is either 1 or -1
   public void tweakTheIndex(int delta) {
     // calculate new index, look for wrap
     tCurIndex += delta;
@@ -280,11 +176,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("curValue", taCurValue[tCurIndex]);
   }
 
-  // Only tweak test values using A, B, X, Y buttons if we are disabled
-  // Y increments value, A decrements value
-  // X goes backwards through list of values, B goes forward
-  /** This function is called periodically when disabled. */
-  @Override
   public void disabledPeriodic() {
 
     // we want distinct press and release of the X-box buttons
@@ -314,10 +205,8 @@ public class Robot extends TimedRobot {
       }
     }
     if (xEnabled && Controller[kDriver].getRawButton(kXButton)) {
-      if (climberSwitchClosed.get() == false) {
         xEnabled = false;
-      tweakTheIndex(-1); // decrease
-      }
+      tweakTheIndex(-1); // decrease by 1
       
     } else {
       if (!Controller[kDriver].getRawButton(kXButton)) {
@@ -325,5 +214,8 @@ public class Robot extends TimedRobot {
       }
     }
   }
+
+  /** This function is called periodically when disabled. */
+
   
 }
